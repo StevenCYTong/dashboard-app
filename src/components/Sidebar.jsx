38717c07341556320e3447824152299e4c45f8b5
Steven Tong <cytong@gmail.com>
2023-03-08 12:20:04 -0700

    sidebar update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,10 @@ import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
-const sidebar = () => {
-	const activeMenu = true;
+const Sidebar = () => {
+	const { activeMenu, setActiveMenu } = useStateContext();
 	const activeLink =
 		'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-log text-white text-md m-2';
 	const normalLink =
@@ -19,7 +20,7 @@ const sidebar = () => {
 					<div className='flex justify-between items-center'>
 						<Link
 							to='/'
-							onClick={() => {}}
+							onClick={() => setActiveMenu(false)}
 							className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
 						>
 							<SiShopware /> <span>Shoppy</span>
@@ -30,7 +31,9 @@ const sidebar = () => {
 						>
 							<button
 								type='button'
-								onClick={() => {}}
+								onClick={() =>
+									setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+								}
 								className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
 							>
 								<MdOutlineCancel />
@@ -63,4 +66,4 @@ const sidebar = () => {
 	);
 };
 
-export default sidebar;
+export default Sidebar;
